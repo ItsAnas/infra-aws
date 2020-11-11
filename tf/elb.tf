b98c25a9301cc097cd3d0f92002e53e8fccc05b2
Antoine Montes <antoine.montes@epita.fr>
2020-11-11 18:04:24 +0100

    tf: add elb

---
+++ tf/elb.tf
@@ -0,0 +1,30 @@
+resource "aws_elb" "epitweet_elb" {
+  name               = "epitweet-elb"
+  availability_zones = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
+
+
+  listener {
+    instance_port     = 8080
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8080/"
+    interval            = 30
+  }
+
+  instances                   = [aws_instance.epitweet_front.id]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "epitweet-elb"
+  }
+}
